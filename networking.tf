71dd2406ce9628a529ae15264f7aa4c8a6d8bc0e
vaibhav jaiswal <vaibhav572724@gmail.com>
2025-02-19 14:19:51 +0100

    Public RT route modified and only IGW specified explicitly.

--- networking.tf
+++ networking.tf
@@ -58,11 +58,6 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = var.vpc_cidr # Local route for VPC traffic
-    gateway_id = "local"      # "local" target for VPC CIDR
-  }
-
-  route {
     cidr_block = "0.0.0.0/0" # Route all other traffic via the Internet Gateway
     gateway_id = aws_internet_gateway.gw.id
   }
