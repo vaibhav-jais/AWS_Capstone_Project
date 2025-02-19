7adb1fdbea2cd9ea5de253191bda522687f49e7a
vaibhav jaiswal <vaibhav572724@gmail.com>
2025-02-19 13:52:46 +0100

    Public and Private route tables created associated with corresponding subnets

--- networking.tf
+++ networking.tf
@@ -53,3 +53,61 @@ resource "aws_subnet" "private_az2" {
     Name = "private-subnet-az2"
   }
 }
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = var.vpc_cidr # Local route for VPC traffic
+    gateway_id = "local"      # "local" target for VPC CIDR
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0" # Route all other traffic via the Internet Gateway
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+# Private Route Table AZ1
+resource "aws_route_table" "private_az1" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table-az1"
+  }
+}
+
+# Private Route Table AZ2
+resource "aws_route_table" "private_az2" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "private-route-table-az2"
+  }
+}
+
+# Public Subnet Route Table Associations
+resource "aws_subnet_route_table_association" "public_az1" {
+  subnet_id      = aws_subnet.public_az1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet_route_table_association" "public_az2" {
+  subnet_id      = aws_subnet.public_az2.id
+  route_table_id = aws_route_table.public.id
+}
+
+# Private Subnet Route Table Associations
+resource "aws_subnet_route_table_association" "private_az1" {
+  subnet_id      = aws_subnet.private_az1.id
+  route_table_id = aws_route_table.private_az1.id
+}
+
+resource "aws_subnet_route_table_association" "private_az2" {
+  subnet_id      = aws_subnet.private_az2.id
+  route_table_id = aws_route_table.private_az2.id
+}
