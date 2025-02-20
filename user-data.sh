8f8b8c253e92de07e65b93294f5075ac927f0622
vaibhav jaiswal <vaibhav572724@gmail.com>
2025-02-20 13:31:32 +0100

    user data.sh file modified

--- user-data.sh
+++ user-data.sh
@@ -3,15 +3,17 @@
 sudo yum update -y
 sudo yum install -y awslogs
 
+# Get the Instance ID
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+
 # Configuring the awslogs agent
 cat > /etc/awslogs/awslogs.conf <<EOL
 [general]
 state_file = /var/lib/awslogs/agent-state
 
-[${aws_s3_bucket.wordpress_logs.bucket}-wordpress-access]
 file = /var/log/httpd/access_log # Or the correct access log path for your setup
 buffer_duration = 5
-log_stream_name = {instance_id}-access
+log_stream_name = ${INSTANCE_ID}-access
 initial_position = start_of_file
 datetime_format = %Y-%m-%d %H:%M:%S
 log_group_name = wordpress-access-logs
