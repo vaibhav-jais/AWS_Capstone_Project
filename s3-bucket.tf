d20252adae179993e656767860dcb482db677c71
vaibhav jaiswal <vaibhav572724@gmail.com>
2025-02-20 13:50:52 +0100

    Adding S3 policies and Roles to sstore logs in S3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -23,3 +23,51 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
     }
   }
 }
+
+# IAM Policy for EC2 Instances to Write Logs to S3
+resource "aws_iam_policy" "s3_log_write" {
+  name        = "EC2-S3-Log-Write-Policy"
+  description = "Allows EC2 instances to write logs to the WordPress Logs S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.wordpress_logs.arn,
+          "${aws_s3_bucket.wordpress_logs.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# IAM Role for EC2 Instances
+resource "aws_iam_role" "ec2_s3_log_write" {
+  name = "EC2-S3-Log-Write-Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Attach the IAM Policy to the IAM Role
+resource "aws_iam_role_policy_attachment" "ec2_s3_log_write_attachment" {
+  role       = aws_iam_role.ec2_s3_log_write.name
+  policy_arn = aws_iam_policy.s3_log_write.arn
+}
